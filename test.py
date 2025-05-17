d56239957c71ac9c9aca3386ff9508bac7e5f13c
spicyQ <shuparnorahman@gmail.com>
2025-05-17 23:29:55 +0600

    Add test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+degree = input("temp:")
+temp = int(degree)
+if temp > 30:
+    print("its warm drink Blood")
+    print("Wear a cap")
+elif temp > 18:
+    print("Extreme Goth Mommy Weather!")
+    print("get some ciggs, wait in a park to be picked by some bloody huzz!!")
+
+elif temp > 2:
+    print(" WINTER GOTH BITCHES!!!")
+    print("YIPPIE !!!")
+else:
+    print("Shiver Me Timbers")
+    print(" Time to get into bed with a goth Babe")
